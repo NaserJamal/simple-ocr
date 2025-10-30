c37a1719179505835264e6a6d7c4055cde9c673f
Naser Jamal <naser.jamal@openinnovation.ai>
2025-10-30 10:44:26 +0400

    1

--- levels/01-basic-text-extraction/extract.py
+++ levels/01-basic-text-extraction/extract.py
@@ -1,5 +1,5 @@
 import pymupdf
 
-doc = pymupdf.open("../../PDF/1-page-text-img.pdf")
+doc = pymupdf.open("../../PDF/110-pages-text.pdf")
 text = "\n".join(page.get_text() for page in doc)
 open("output/output.txt", "w").write(text)
